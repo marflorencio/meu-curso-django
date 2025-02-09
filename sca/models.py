b30d11597ec248c7721975db13aa4f6d555091c1
Marcelo Florencio <marflorencio@gmail.com>
2025-02-09 17:40:48 -0300

    Meu Curso Django com app SCA, terceiro commit

--- sca/models.py
+++ sca/models.py
@@ -7,7 +7,7 @@ class Usuario(models.Model):
     login = models.CharField(max_length=20)
     senha = models.CharField(max_length=20)
     nome = models.CharField(max_length=75)
-    situacao = models.CharField(max_length=1, choices=SITUACAO)
+    situacao = models.CharField(max_length=1, choices=SITUACAO, default="A")
     data_alteracao = models.DateTimeField('data alteração')
 
     def salvar(self):
