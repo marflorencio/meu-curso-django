6a6ac660f3109c6c217766c92031d28013b9bbe4
Marcelo Florencio <marflorencio@gmail.com>
2025-02-09 20:03:58 -0300

    Meu Curso Django com app SCA, quinto commit

--- sca/models.py
+++ sca/models.py
@@ -3,10 +3,11 @@ from django.db import models
 from django.utils import timezone
 
 class Usuario(models.Model):
-    login = models.CharField(max_length=20)
+    SITUACAO_CHOICES = {"A": "Ativo", "I" : "Inativo", "S": "Suspenso", "O":"outro",}
+    login = models.CharField(max_length=20, unique=True)
     senha = models.CharField(max_length=20)
     nome = models.CharField(max_length=75)
-    situacao = models.CharField(max_length=1)
+    situacao = models.CharField(max_length=1, choices=SITUACAO_CHOICES, default="A")
     data_alteracao = models.DateTimeField('data alteração')
 
     def salvar(self):
